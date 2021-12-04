4d05683894d438ee3ba7a6f4c1cce64d00410bf8
wirgy <tima.homyakov26072002@gmail.com>
2021-12-04 16:58:13 +0800

    Create a list of random particles

--- CourseWork/Form1.cs
+++ CourseWork/Form1.cs
@@ -12,12 +12,25 @@ namespace CourseWork
 {
     public partial class Form1 : Form
     {
+
+        List<Particle> particles = new List<Particle>();
         public Form1()
         {
             InitializeComponent();
 
             // привязал изображение
             picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            // генерирую 500 частиц
+            for (var i = 0; i < 500; ++i)
+            {
+                var particle = new Particle();
+                // переношу частицы в центр изображения
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+                // добавляю список
+                particles.Add(particle);
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
