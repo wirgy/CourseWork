38258f9d6cd5af2b9b20604d85a11c4a0a6ed53b
wirgy <tima.homyakov26072002@gmail.com>
2021-12-04 17:06:59 +0800

    Add timer

--- CourseWork/Form1.Designer.cs
+++ CourseWork/Form1.Designer.cs
@@ -29,7 +29,9 @@ namespace CourseWork
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.picDisplay = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.picDisplay)).BeginInit();
             this.SuspendLayout();
             // 
@@ -40,6 +42,12 @@ namespace CourseWork
             this.picDisplay.Size = new System.Drawing.Size(776, 426);
             this.picDisplay.TabIndex = 0;
             this.picDisplay.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 40;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
@@ -58,6 +66,7 @@ namespace CourseWork
         #endregion
 
         private System.Windows.Forms.PictureBox picDisplay;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- CourseWork/Form1.cs
+++ CourseWork/Form1.cs
@@ -37,5 +37,28 @@ namespace CourseWork
         {
 
         }
+
+        int counter = 0; // добавлю счетчик чтобы считать вызовы функции
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter++; // увеличиваю значение счетчика каждый вызов
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.White); // добавил очистку
+                // рисую на изображении сколько насчитал
+                g.DrawString(
+                    counter.ToString(), // значения счетчика в виде строки
+                    new Font("Arial", 12), // шрифт
+                    new SolidBrush(Color.Black), // цвет
+                    new PointF
+                    { // по центру экрана
+                    X = picDisplay.Image.Width / 2,
+                        Y = picDisplay.Image.Height / 2
+                    }
+                );
+            }
+            // обновить picDisplay
+            picDisplay.Invalidate();
+        }
     }
 }
